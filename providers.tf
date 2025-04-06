e1bb37c8d40f0b8d2e809b8893e36b5f1450e0ef
ravisivaji12 <ravi.sivaji@outlook.com>
2025-04-05 22:42:01 -0400

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,14 @@
+
+#############################################################################
+#                           Azure Providers
+#############################################################################
+
+#               McCain MF-MD-NON-Prod Subscription
+
+provider "azurerm" {
+  features {}
+  client_id       = ""
+  client_secret   = ""
+  subscription_id = ""
+  tenant_id       = ""
+}
